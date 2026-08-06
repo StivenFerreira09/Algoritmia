72a949bcd10e729c7d2a24786ee446c691384e8a
StivenFerreira09 <stiven.x.cn@gmail.com>
2026-08-05 19:11:28 -0500

    Add files via upload

---
+++ Carpeta POO/01_EjercicioLibro/Libro.java
@@ -0,0 +1,17 @@
+package com.mycompany.principal;
+
+public class Libro {
+
+    String titulo;
+    String autor;
+    int numeroPaginas;
+
+    public void método() {
+
+        System.out.println("Titulo: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Numero de paginas: " + numeroPaginas);
+
+    }
+
+}
---
+++ Carpeta POO/01_EjercicioLibro/Principal.java
@@ -0,0 +1,17 @@
+package com.mycompany.principal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Libro libro = new Libro();
+
+        libro.titulo = "La metamorfosis";
+        libro.autor = " Franz Kafka";
+        libro.numeroPaginas = 70;
+
+        libro.método();
+
+    }
+
+}
